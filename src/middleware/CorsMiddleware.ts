73c0950652ea4eae262a90896fc7396d72b7d5c5
Chuli jimmi Manurung <jimmi.ext@gmail.com>
2021-04-15 05:52:48 +0800

    fix cors origin

--- src/middleware/CorsMiddleware.ts
+++ src/middleware/CorsMiddleware.ts
@@ -1,9 +1,10 @@
-import cors, { CorsOptionsDelegate } from 'cors'
+import cors, { CorsOptionsDelegate } from 'cors';
 
 const allowedOrigin = [
   'https://chat-backend-jimmi.herokuapp.com',
   'localhost:5000',
-]
+  'https://hungry-nobel-29f7e8.netlify.app',
+];
 
 const corsOptionResponse = {
   allowedHeaders: [
@@ -16,24 +17,27 @@ const corsOptionResponse = {
   credentials: true,
   methods: 'GET,POST',
   origin: '',
-}
+};
 
 const options: CorsOptionsDelegate = (requestOrigin, callback) => {
-  const host: string = requestOrigin.headers.host || ''
-  const message = 'You have problem with CORS policy'
+  const host: string = requestOrigin.headers.host || '';
+  const message = 'You have problem with CORS policy';
   if (allowedOrigin.indexOf(host) === -1) {
-    corsOptionResponse.origin = host
-    callback(new Error(message), corsOptionResponse)
+    corsOptionResponse.origin = host;
+    callback(new Error(message), corsOptionResponse);
   } else {
-    corsOptionResponse.origin = host
-    callback(null, corsOptionResponse)
+    corsOptionResponse.origin = host;
+    callback(null, corsOptionResponse);
   }
-}
+};
 
-export const RestCors = cors(options)
+export const RestCors = cors(options);
 export const SocketCors = {
-  origin: 'http://localhost:8000',
+  origin:
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8000'
+      : 'https://hungry-nobel-29f7e8.netlify.app',
   methods: ['GET', 'POST'],
   allowedHeaders: ['my-custom-header'],
   credentials: true,
-}
+};
